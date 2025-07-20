cb511935ef4c9be6aadf7bd3674c600b088c26cf
Eksang <elkanahkibet6@gmail.com>
2025-07-20 16:15:13 +0300

    added a play pause functionality

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,5 +1,22 @@
-const PlayPause = () => (
-  <div></div>
-);
+import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa';
+
+function PlayPause({ isPlaying, activeSong, song, handlePause, handlePlay }) {
+  const isCurrentSongPlaying = isPlaying && activeSong?.title === song.title;
+
+  return isCurrentSongPlaying ? (
+    <FaPauseCircle
+      size={35}
+      className="text-gray-300"
+      onClick={handlePause}
+    />
+  ) : (
+    <FaPlayCircle
+      size={35}
+      className="text-gray-300"
+      onClick={handlePlay}
+    />
+  );
+}
 
 export default PlayPause;
+
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,23 +1,31 @@
 import { Error,Loader, SongCard } from "../components";
 import {genres } from '../assets/constants'
 import {useGetTopChartsQuery} from '../redux/services/shazam'
+import { selectGenreListId } from '../redux/features/playerSlice';
+import { useDispatch, useSelector } from 'react-redux';
+
 
 function Discover (){
+    const dispatch = useDispatch();
     const {data, isFetching, error}= useGetTopChartsQuery();
+    const { activeSong, isPlaying } = useSelector((state) => state.player);
+    const { genreListId } = useSelector((state) => state.player);
+    
     if(isFetching) return <Loader title="Loading songs..."/>;
 
     if(error) return <Error />;
+
+    const genreTitle = genres.find(({ value }) => value === genreListId)?.title;
 
 
 
     return(
         <div className="flex flex-col">
             <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
-                <h2 className=" font-bold text-3xl text-white text-left">Discover</h2>
-                <select onChange={()=> {}}
-                    value="" className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5">
-                        {genres.map((genre) => <option key={genre.value} value={genre.value}>{genre.title}</option>)}
-                    </select>
+                <h2 className=" font-bold text-3xl text-white text-left">Discover {genreTitle}</h2>
+                <select onChange={(e) => dispatch(selectGenreListId(e.target.value))} value={genreListId || 'pop'} className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5">
+                    {genres.map((genre) => <option key={genre.value} value={genre.value}>{genre.title}</option>)}
+                </select>
             </div>
             <div className="flex flex-wrap sm:justify-start justify-center gap-8">
                 {data?.map((song,i)=>(
@@ -25,6 +33,9 @@ function Discover (){
                        key={song.key}
                        song={song}
                        i={i}
+                       isPlaying={isPlaying}
+                       activeSong={activeSong}
+                       data={data}
                     />)
                     )
                 }
